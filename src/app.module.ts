bf133b201da215af20e56ecb6ed2de6e69e9bc5d
Abel Lavieri <abel.lavieri@gmail.com>
2024-07-07 12:47:32 -0400

    Add Task module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,19 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
 import { ConfigModule } from '@nestjs/config'
-import { ThrottlerModule } from '@nestjs/throttler'
-import { UserModule } from './user/user.module'
 import { MongooseModule } from '@nestjs/mongoose'
+import { TaskModule } from './task/task.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TaskListModule } from './task-list/task-list.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    ThrottlerModule.forRoot({ limit: 10, ttl: 60 }),
-    MongooseModule.forRoot(process.env.DATABASE_URI, {
-      dbName: process.env.DATABASE_NAME,
-      auth: {
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASS,
-      },
-    }),
-
-    // feature module
-    UserModule,
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    TaskModule,
+    TaskListModule,
   ],
   controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
