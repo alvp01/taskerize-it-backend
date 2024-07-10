9146b80e46cce2e845b873809067960154ac42a3
Abel Lavieri <abel.lavieri@gmail.com>
2024-07-10 05:12:04 -0400

    Add return types to the task controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Post, Body, Get, Put, Delete, ValidationPipe, Param } from 
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './task.schema';
 
 
 @Controller('tasks')
@@ -9,12 +10,15 @@ export class TaskController {
   constructor(private TaskService: TaskService) {}
 
   @Post()
-  async create(@Body(new ValidationPipe()) task: CreateTaskDto){
+  async create(
+    @Body(new ValidationPipe())
+    task: CreateTaskDto
+  ): Promise<Task> {
     return await this.TaskService.create(task);
   }
 
   @Get()
-  async index(){
+  async index(): Promise<Task[]> {
     return await this.TaskService.findAll();
   }
 
@@ -22,17 +26,17 @@ export class TaskController {
   async update(
     @Param('id') id: string,
     @Body(new ValidationPipe()) task: UpdateTaskDto
-  ){
+  ): Promise<Task> {
     return await this.TaskService.update(id, task);
   }
 
   @Get(':id')
-  async show(@Param('id') id: string){
+  async show(@Param('id') id: string): Promise<Task> {
     return await this.TaskService.findOne(id);
   }
 
   @Delete(':id')
-  async destroy(@Param('id') id: string){
+  async destroy(@Param('id') id: string): Promise<Task> {
     return await this.TaskService.delete(id);
   }
 }
